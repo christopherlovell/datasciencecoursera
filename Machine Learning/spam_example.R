1e8ca3251ce60924d523ce419e87e2b9e04cc199
Chris <chrisl1991@hotmail.co.uk>
2014-10-20 18:24:18 +0100

    Update spam_example.R

--- Machine Learning/spam_example.R
+++ Machine Learning/spam_example.R
@@ -7,7 +7,14 @@ inTrain <- createDataPartition(y=spam$type,p=0.75,list=FALSE)
 
 training <- spam[inTrain,]
 testing <- spam[-inTrain,]
-
 dim(training)
 
+####
+
+set.seed(334)
+
+# ~. in the type part of the function call represents using all types in the data frame to train the model
+# glm is one of many models
+modelFit <- train(type ~.,data=training,method="glm")
+modelFit
 
