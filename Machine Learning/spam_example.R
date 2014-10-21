d850cbc163e7ce0f3bc4675690bc1933431156d0
Chris <chrisl1991@hotmail.co.uk>
2014-10-21 17:58:57 +0100

    Update spam_example.R

--- Machine Learning/spam_example.R
+++ Machine Learning/spam_example.R
@@ -28,6 +28,9 @@ predictions
 
 confusionMatrix(predictions,testing$type)
 
+
+####
+#### TRAINING OPTIONS
 ####
 
 # k-fold example
@@ -56,3 +59,36 @@ names(folds)
 
 folds$train[[2]]
 folds$test[[2]]
+
+args(train.default)
+args(trainControl)
+
+####
+#### PLOTTING PREDICTORS
+####
+
+library(ISLR)
+library(ggplot2)
+library(caret)
+
+data(Wage)
+summary(Wage)
+
+inTrain <- createDataPartition(y=Wage$wage,p=0.7,list=FALSE)
+
+training <- Wage[inTrain,]
+testing <- Wage[-inTrain,]
+
+dim(training)
+dim(testing)
+
+# feature plot
+
+featurePlot(x=training[,c("age","education","jobclass")],
+            y=training$wage,
+            plot="pairs")
+
+
+
+
+
