4e5fb7e36ba1f16d765269c5fbc51264b7ae1e27
polyphant <polyphant@hotmail.com>
2014-11-24 14:20:02 +0000

    PCA update

--- Machine Learning/Preprocessing with PCA.R
+++ Machine Learning/Preprocessing with PCA.R
@@ -27,9 +27,48 @@ plot(spam[,34],spam[,32])
 
 # how can we combine these but retain as much information as possible?
 # we could rotate the plot:
-X<- 0.71*training$num415 + 0.71*training$num857
-Y<- 0.71*training$num415 - 0.71*training$num857
+X <- 0.71*training$num415 + 0.71*training$num857
+Y <- 0.71*training$num415 - 0.71*training$num857
 plot(X,Y)
+# adding the variables caches most of the info (most are constantat 0 on Y axis)
+
+smallSpam <- spam[,c(34,32)]
+prComp <- prcomp(smallSpam)
+?prcomp
+plot(prComp$x[,1],prComp$x[,2])
+
+# equal to values shown before
+prComp$rotation
+
+# do for more vaiables...
+# first assign color depending on type
+typeColor <- ((spam$type=="spam")*1 + 1)
+# apply log to make the data look more gaussian (often necessary for PCA)
+prComp <- prcomp(log10(spam[,-58]+1))
+plot(prComp$x[,1],prComp$x[,2],col=typeColor,xlab="PC1",ylab="PC2")
+
+prComp$rotation
+
+
+# can do the same with the Caret package using the preProcess function
+preProc <- preProcess(log10(spam[,-58]+1),method="pca",pcaComp=2)
+spamPC <- predict(preProc,log10(spam[,-58]+1))
+plot(spamPC[,1],spamPC[,2],col=typeColor)
+
+
+preProc <- preProcess(log10(spam[,-58]+1),method="pca",pcaComp=2)
+trainPC <- predict(preProc,log10(training[,-58]+1))
+# train model using PCs
+modelFit <- train(training$type ~ .,method="glm",data=trainPC)
+
+# pass preProc calculated on training data to test
+testPC <- predict(preProc,log10(testing[,-58]+1))
+confusionMatrix(testing$type,predict(modelFit,testPC))
+
+
+# can also combine predict into training exercise:
+modelFit <- train(training$type ~ .,method="glm",preProcess="pca",data=training)
+confusionMatrix(testing$type,predict(modelFit,testing))
 
 
 
