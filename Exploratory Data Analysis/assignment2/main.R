cbf53b048cbf5275a544c9622ca197698176ab95
Polyphant1 <chrisl1991@hotmail.co.uk>
2015-02-10 08:09:17 +0000

    completed Exploratory data analysis ex.2

--- Exploratory Data Analysis/assignment2/main.R
+++ Exploratory Data Analysis/assignment2/main.R
@@ -1,6 +1,7 @@
 
 library(plyr)
 library(ggplot2)
+library(gridExtra)
 
 wd<-"C://Users//Chris//Documents//Data Science Files//datasciencecoursera//Exploratory Data Analysis//assignment2"
 setwd(wd)
@@ -29,23 +30,61 @@ p1<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=type,colour=type))+geom_lin
 p1
 
 ## Plot 4
-classification.code.coal<-subset(classification.code,classification.code$EI.Sector %in% c("Fuel Comb - Comm/Institutional - Coal","Fuel Comb - Industrial Boilers, ICEs - Coal ","Fuel Comb - Comm/Institutional - Coal"))
+coal_sources<-c("Fuel Comb - Comm/Institutional - Coal"
+                ,"Fuel Comb - Industrial Boilers, ICEs - Coal "
+                ,"Fuel Comb - Comm/Institutional - Coal")
+classification.code.coal<-subset(classification.code,classification.code$EI.Sector %in% coal_sources)
+
 scc.pm25.coal<-scc.pm25[scc.pm25$SCC %in% classification.code.coal$SCC,]
 
-year.sum<-ddply(scc.pm25.coal,.variables = c("SCC","year"),numcolwise(sum))
-year.sum.total<-ddply(scc.pm25.coal,.variables = c("year"),numcolwise(sum))
-year.sum.total<-cbind("Total",year.sum.total)
-names(year.sum.total)<-names(year.sum)
-year.sum<-rbind(year.sum,year.sum.total)
+year.sum<-ddply(scc.pm25.coal,.variables = c("year","type"),numcolwise(sum))
+# year.sum.total<-ddply(scc.pm25.coal,.variables = c("year"),numcolwise(sum))
+# year.sum.total<-cbind("Total",year.sum.total)
+# names(year.sum.total)<-names(year.sum)
+# year.sum<-rbind(year.sum,year.sum.total)
 
-p1<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=SCC,colour=SCC))+geom_line(stat="identity")
+p1<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=type,colour=type))
+#p1<-p1+geom_line(stat="identity")
+p1<-p1+geom_area(position = 'stack')
 p1
 
 ## Plot 5
-classification.code.coal<-subset(classification.code,classification.code$EI.Sector == "Fuel Comb - Comm/Institutional - Coal")
+motor_vehicle_sources<-c("Mobile - On-Road Gasoline Light Duty Vehicles"
+                         ,"Mobile - On-Road Gasoline Heavy Duty Vehicles"
+                         ,"Mobile - On-Road Diesel Light Duty Vehicles"
+                         ,"Mobile - On-Road Diesel Heavy Duty Vehicles")
+classification.code.motor<-subset(classification.code,classification.code$EI.Sector %in% motor_vehicle_sources)
+scc.pm25.baltimore_city.motor<-subset(scc.pm25,fips=="24510" & SCC %in% classification.code.motor$SCC)
 
+year.sum<-ddply(scc.pm25.baltimore_city.motor,.variables = "year",numcolwise(sum))
+p1<-ggplot(data=year.sum,aes(x=year,y=Emissions))+geom_line(stat="identity")
+p1
 
+## Plot 6
+motor_vehicle_sources<-c("Mobile - On-Road Gasoline Light Duty Vehicles"
+                         ,"Mobile - On-Road Gasoline Heavy Duty Vehicles"
+                         ,"Mobile - On-Road Diesel Light Duty Vehicles"
+                         ,"Mobile - On-Road Diesel Heavy Duty Vehicles")
+classification.code.motor<-subset(classification.code,classification.code$EI.Sector %in% motor_vehicle_sources)
 
+scc.pm25.motor<-subset(scc.pm25,fips %in% c("06037","24510") & SCC %in% classification.code.motor$SCC)
 
+year.sum<-ddply(scc.pm25.motor,.variables = c("fips","year"),numcolwise(sum))
+
+year.sum[year.sum$fips=="06037","index"]<-apply(year.sum[year.sum$fips=="06037",],1,function(x) as.numeric(x[3])/year.sum[year.sum$year=="1999" & year.sum$fips=="06037",3])
+year.sum[year.sum$fips=="24510","index"]<-apply(year.sum[year.sum$fips=="24510",],1,function(x) as.numeric(x[3])/year.sum[year.sum$year=="1999" & year.sum$fips=="24510",3])
+
+p1<-ggplot(data=year.sum,aes(x=year,y=index,fill=fips,colour=fips))
+p1<-p1+geom_line(stat="identity")
+p1<-p1+scale_colour_discrete(name="County",
+                           breaks=c("06037", "24510"),
+                           labels=c("Los Angeles County","Baltimore City"))
+
+p2<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=fips,colour=fips))+geom_line(stat="identity")
+p2<-p2+scale_colour_discrete(name="County",
+                             breaks=c("06037", "24510"),
+                             labels=c("Los Angeles County","Baltimore City"))
+
+grid.arrange(p2,p1)
 
 
