b5786a7c111916791483666ddc5e080ec30013aa
Polyphant1 <chrisl1991@hotmail.co.uk>
2015-02-12 22:05:51 +0000

    completed assignment 2

--- Exploratory Data Analysis/assignment2/main.R
+++ Exploratory Data Analysis/assignment2/main.R
@@ -6,6 +6,10 @@ library(gridExtra)
 wd<-"C://Users//Chris//Documents//Data Science Files//datasciencecoursera//Exploratory Data Analysis//assignment2"
 setwd(wd)
 
+library(plyr)
+library(ggplot2)
+library(gridExtra)
+
 file.zip<-download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",destfile="FNEI_data.zip")
 
 unzip(zipfile = "FNEI_data.zip")
@@ -15,21 +19,30 @@ scc.pm25<-readRDS("summarySCC_PM25.rds")
 
 
 ## Plot 1
+png(filename = "question1.png",width = 480,height = 480)
 year.sum<-ddply(scc.pm25,"year",numcolwise(sum))
-plot(year.sum,type = 'l')
+plot(year.sum,type = 'l',main="PM-2.5 emissions in the US, \n1999-2008")
+dev.off()
 
 ## Plot 2
+png(filename = "question2.png",width = 480,height = 480)
 scc.pm25.baltimore_city<-subset(scc.pm25,fips=="24510")
 year.sum<-ddply(scc.pm25.baltimore_city,"year",numcolwise(sum))
-plot(year.sum,type = 'p')
+plot(year.sum,type = 'l',main="PM-2.5 emissions in Baltimore City, Maryland, \n1999-2008")
+dev.off()
 
 ## Plot 3
+png(filename = "question3.png",width = 480,height = 480)
 scc.pm25.baltimore_city<-subset(scc.pm25,fips=="24510")
 year.sum<-ddply(scc.pm25.baltimore_city,.variables = c("year","type"),numcolwise(sum))
-p1<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=type,colour=type))+geom_line(stat="identity")
+p1<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=type,colour=type))
+p1<-p1+geom_line(stat="identity")
+p1<-p1+ggtitle("PM-2.5 sources in Baltimore City, Maryland, \n1999-2008")
 p1
+dev.off()
 
 ## Plot 4
+png(filename = "question4.png",width = 480,height = 480)
 coal_sources<-c("Fuel Comb - Comm/Institutional - Coal"
                 ,"Fuel Comb - Industrial Boilers, ICEs - Coal "
                 ,"Fuel Comb - Comm/Institutional - Coal")
@@ -38,17 +51,16 @@ classification.code.coal<-subset(classification.code,classification.code$EI.Sect
 scc.pm25.coal<-scc.pm25[scc.pm25$SCC %in% classification.code.coal$SCC,]
 
 year.sum<-ddply(scc.pm25.coal,.variables = c("year","type"),numcolwise(sum))
-# year.sum.total<-ddply(scc.pm25.coal,.variables = c("year"),numcolwise(sum))
-# year.sum.total<-cbind("Total",year.sum.total)
-# names(year.sum.total)<-names(year.sum)
-# year.sum<-rbind(year.sum,year.sum.total)
 
 p1<-ggplot(data=year.sum,aes(x=year,y=Emissions,fill=type,colour=type))
-#p1<-p1+geom_line(stat="identity")
 p1<-p1+geom_area(position = 'stack')
+p1<-p1+ggtitle("PM-2.5 emissions from \ncoal combustion-related sources,\n 1999-2008")
 p1
 
+dev.off()
+
 ## Plot 5
+png(filename = "question5.png",width = 480,height = 480)
 motor_vehicle_sources<-c("Mobile - On-Road Gasoline Light Duty Vehicles"
                          ,"Mobile - On-Road Gasoline Heavy Duty Vehicles"
                          ,"Mobile - On-Road Diesel Light Duty Vehicles"
@@ -58,9 +70,13 @@ scc.pm25.baltimore_city.motor<-subset(scc.pm25,fips=="24510" & SCC %in% classifi
 
 year.sum<-ddply(scc.pm25.baltimore_city.motor,.variables = "year",numcolwise(sum))
 p1<-ggplot(data=year.sum,aes(x=year,y=Emissions))+geom_line(stat="identity")
+p1<-p1+ggtitle("PM-2.5 emissions from motor vehicle sources\n in Baltimore City, Maryland, 1999-2008")
 p1
 
+dev.off()
+
 ## Plot 6
+png(filename = "question6.png",width = 480,height = 480)
 motor_vehicle_sources<-c("Mobile - On-Road Gasoline Light Duty Vehicles"
                          ,"Mobile - On-Road Gasoline Heavy Duty Vehicles"
                          ,"Mobile - On-Road Diesel Light Duty Vehicles"
@@ -87,4 +103,4 @@ p2<-p2+scale_colour_discrete(name="County",
 
 grid.arrange(p2,p1)
 
-
+dev.off()
